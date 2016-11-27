c53ec987c001251ac34e600a745600765c9db9f9
regigold <tbateman88@gmail.com>
2016-11-26 21:34:56 -0800

    styles updated... nav-animation.js needs work...

---
+++ scripts/nav-animation.js
@@ -0,0 +1,25 @@
+// javascript document
+
+$( document ).ready(function() {
+    
+    $('.menu a').css('height';
+
+    $('.sections').click(function(){
+        $('.menu').toggleClass('open');
+
+});    
+    
+    
+//    $('.menu').removeClass();
+//$('nav li').hover(
+//  function () {
+//    $('ul', this).stop().slideDown(100);
+//  },
+//  function () {
+//    $('ul', this).stop().slideUp(100);
+//  }
+//);
+    
+    
+    
+}); 
