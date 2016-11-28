816b3d053c7f2eff227dc5cdbd32852a7e8ca3c3
Tevis Bateman <tbateman88@gmail.com>
2016-11-27 22:40:28 -0800

    trending and news sections added... still need to add one more news section before beginning responsive

--- scripts/nav-animation.js
+++ scripts/nav-animation.js
@@ -1,25 +1,9 @@
 // javascript document
 
 $( document ).ready(function() {
-    
-    $('.menu a').css('height';
-
+        
     $('.sections').click(function(){
-        $('.menu').toggleClass('open');
-
-});    
-    
-    
-//    $('.menu').removeClass();
-//$('nav li').hover(
-//  function () {
-//    $('ul', this).stop().slideDown(100);
-//  },
-//  function () {
-//    $('ul', this).stop().slideUp(100);
-//  }
-//);
-    
-    
-    
+        $('.menu').toggleClass('open')
+    });
+      
 }); 
