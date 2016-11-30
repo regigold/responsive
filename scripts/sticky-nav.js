0782a9fd1930c7380e5f7d7c62e3bcccc2f8c714
Tevis Bateman <A00954003@ad.bcit.ca>
2016-11-29 19:23:21 -0800

    media queries started... half way there... mobile views still need to be added.

--- scripts/sticky-nav.js
+++ scripts/sticky-nav.js
@@ -1,14 +1,40 @@
 // javascript document
 
-var scrollAmount = 200; 
-          var $window = $(window);
-          $window.on('scroll', function(){
-  if($(window).scrollTop()>=scrollAmount && !$('header').hasClass('fixed')){
-    $('header').addClass('fixed'); 
-    $('.wrapper').css('margin-top', '290px');
-  }
-  else if($(window).scrollTop()<=scrollAmount && $('header').hasClass('fixed')){
-     $('header').removeClass('fixed') 
-     $('.wrapper').css('margin-top', '0px');
-  }
-}); 
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('header').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('header').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    
+    lastScrollTop = st;
+} 
